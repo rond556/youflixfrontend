3d8e7e97f0efca8702762677842f1b69877b1528
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-12-20 13:39:09 -0500

    fixed and working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { VideoComponent } from './video/video.component';
 import { UploadsComponent } from './uploads/uploads.component';
-import { VideoComponent} from './video/video.component';
 
 
 
-const routes: Routes = [  
+const routes: Routes = [ 
+  { path: '', redirectTo: '/video', pathMatch: 'full' },
+  { path: 'video', component: VideoComponent },
+  { path: 'uploads', component: UploadsComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { UploadsComponent } from './uploads/uploads.component';
+import { VideoComponent } from './video/video.component';
+
+import { AppRoutingModule }     from './app-routing.module';
+
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
-    UploadsComponent
+    UploadsComponent,
+    VideoComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
