aabe911e48c0bddeb46279870fb6f5b2743f907c
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-12-21 15:16:08 -0500

    updates

--- src/app/uploads/uploads.component.ts
+++ src/app/uploads/uploads.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import * as AWS from 'aws-sdk';
+
+
 
 @Component({
   selector: 'app-uploads',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./uploads.component.scss']
 })
 export class UploadsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  
+  ngOnInit(){
+  }
+  
+  fileEvent(fileInput: any) {
+    const AWSService = AWS;
+    const region = 'us-east-1';
+    const bucketName = 'zcw-group-videos';
+    const IdentityPoolId = 'us-east-1:67bef216-708b-478e-ab63-81881097adaa';
+    const file = fileInput.target.files[0];
+  //Configures the AWS service and initial authorization
+    AWSService.config.update({
+      region: region,
+      credentials: new AWSService.CognitoIdentityCredentials({
+        IdentityPoolId: IdentityPoolId
+      })
+    });
+  //adds the S3 service, make sure the api version and bucket are correct
+    const s3 = new AWSService.S3({
+      apiVersion: '2006-03-01',
+      params: { Bucket: bucketName}
+    });
+  
+    s3.upload({ Key: file.name, Bucket: bucketName, Body: file, ACL: 'public-read'}, function (err, data) {
+     if (err) {
+       console.log(err, 'there was an error uploading your file');
+     }
+   });
   }
 
 }
