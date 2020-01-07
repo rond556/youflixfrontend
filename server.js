5bb4c630fe6a0a398cc6e2143dae477272368791
michael-doll <MWDoll88@gmail.com>
2020-01-07 17:23:31 -0500

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-Install express server
+//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -13,7 +13,7 @@ res.sendFile(path.join(__dirname,'/dist/angular-youflix-videoapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
 
 
 
