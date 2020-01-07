2bb998eff2d49a620bee428990b1338a7f856dc8
michael-doll <MWDoll88@gmail.com>
2020-01-07 14:36:39 -0500

    Update Server.js

--- server.js
+++ server.js
@@ -7,16 +7,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-youflix-videoapp'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname,'/dist/angular-youflix-videoapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 8080);
