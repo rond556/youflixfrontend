f5f9fb37b4e526ca7849b2c7c193d30b4792abd6
michael-doll <MWDoll88@gmail.com>
2020-01-07 13:40:53 -0500

    Access-Control-Allow-Origin

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/angular-youflix-videoapp'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/*', function(req,res) {
     
